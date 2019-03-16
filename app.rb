2f252ec5287030ee7880e7c8a5f7c3edae3c0b74
yw0920 <ywsep20@gmail.com>
2019-03-16 08:35:56 +0000

    post追加

--- app.rb
+++ app.rb
@@ -8,5 +8,15 @@ get '/' do
 end
 
 post '/' do
+  client = Mysql2::Client.new(host: 'localhost', username: 'root', database: 'booklist', encoding: 'utf8')
+
+  # 書籍タイトルの保存
+  book_title = params['book_title']
+  statement = client.prepare('INSERT INTO books (book_title) VALUES(?)')
+  statement.execute(book_title)
+
+  # 書籍タイトルの一覧取得
+  @records = client.query("SELECT * FROM books ORDER BY created_at DESC")
+
   erb :booklist
 end
